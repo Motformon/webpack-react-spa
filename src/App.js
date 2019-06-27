dbe0db960663993130760772c4ea6184a27ebb8c
motformon <motformon@gmail.com>
2019-06-27 21:52:57 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Test from './Test';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="footer">
+				<Test/>
+			</div>
+		) 
+	}
+}
---
+++ src/modules/header.component.js
@@ -0,0 +1,3 @@
+import _ from 'lodash';
+
+console.log(_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 }));
